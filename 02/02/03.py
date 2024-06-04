df1169e4e7449cb1be1e2510cda5edfe820fa7b9
pych1971 <pych1971@gmail.com>
2024-06-04 13:58:49 +0500

    2024-06-04-13-58

--- 02/02/03.py
+++ 02/02/03.py
@@ -29,7 +29,68 @@ class Stack:
         if self.top is None:
             self.top = obj
         else:
+            i = j = self.top
+            while i is not None:
+                j = i
+                i = i.next
+            else:
+                j.next = obj
 
     def pop(self):
+        if self.top is not None and self.top.next is not None:
+            i = j = self.top
+            while i.next is not None:
+                j = i
+                i = i.next
+            else:
+                j.next = None
+            return i
+        else:
+            j = self.top
+            self.top = None
+            return j
 
     def get_data(self):
+        res = []
+        i = self.top
+        while i is not None:
+            res.append(i.data)
+            i = i.next
+        return res
+
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.push(StackObj("obj_2"))
+s.push(StackObj("obj_3"))
+s.pop()
+
+res = s.get_data()
+assert res == ["obj_1", "obj_2"], f"метод get_data вернул неверные данные: {res}"
+assert s.top == top, "атрибут top объекта класса Stack содержит неверное значение"
+
+h = s.top
+while h:
+    res = h.data
+    h = h.next
+
+s = Stack()
+top = StackObj("obj_1")
+s.push(top)
+s.pop()
+assert s.get_data() == [], f"метод get_data вернул неверные данные: {s.get_data()}"
+
+n = 0
+h = s.top
+while h:
+    h = h.next
+    n += 1
+
+assert n == 0, "при удалении всех объектов, стек-подобная стурктура оказалась не пустой"
+
+s = Stack()
+top = StackObj("name_1")
+s.push(top)
+obj = s.pop()
+assert obj == top, "метод pop() должен возвращать удаляемый объект"
